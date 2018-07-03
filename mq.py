acc887d74ef0c3c90515989396cb415dd9638e66
Pavel Kuzmenko <kyzmenko.pavel@gmail.com>
2018-07-03 17:07:01 +0300

    add garanted adv view

--- mq.py
+++ mq.py
@@ -3,57 +3,44 @@ from amqplib import client_0_8 as amqp
 
 
 class MQ():
-    def _get_worker_channel(self):
-        ''' Подключается к брокеру mq '''
-        conn = amqp.Connection(host='amqp.yottos.com',
+    def __init__(self):
+        self.conn = amqp.Connection(host='amqp.yottos.com',
                                userid='worker',
                                password='worker',
                                virtual_host='worker',
                                insist=True)
-        ch = conn.channel()
-        ch.exchange_declare(exchange="getmyad", type="topic", durable=True, auto_delete=False, passive=False)
-        return ch
+        self.ch = self.conn.channel()
+        self.ch.exchange_declare(exchange="getmyad", type="topic", durable=True, auto_delete=False, passive=False)
+
+    def __del__(self):
+        self.ch.close()
 
     def rating_informer_update(self, guid_int):
-        ch_worker = self._get_worker_channel()
         msg = amqp.Message(str(guid_int))
-        ch_worker.basic_publish(msg, exchange='getmyad', routing_key='informer.updateRating')
-        ch_worker.close()
+        self.ch.basic_publish(msg, exchange='getmyad', routing_key='informer.updateRating')
 
     def rating_informer_delete(self, guid_int):
-        ch_worker = self._get_worker_channel()
         msg = amqp.Message(str(guid_int))
-        ch_worker.basic_publish(msg, exchange='getmyad', routing_key='informer.updateRating')
-        ch_worker.close()
+        self.ch.basic_publish(msg, exchange='getmyad', routing_key='informer.updateRating')
 
     def offer_update(self, offer_Id, campaign_id):
-        ch_worker = self._get_worker_channel()
         msg = 'Offer:%s,Campaign:%s' % (offer_Id, campaign_id)
         msg = amqp.Message(msg)
-        ch_worker.basic_publish(msg, exchange='getmyad', routing_key='advertise.update')
-        ch_worker.close()
+        self.ch.basic_publish(msg, exchange='getmyad', routing_key='advertise.update')
 
     def offer_delete(self, offer_Id, campaign_id):
-        ch_worker = self._get_worker_channel()
         msg = 'Offer:%s,Campaign:%s' % (offer_Id, campaign_id)
         msg = amqp.Message(msg)
-        ch_worker.basic_publish(msg, exchange='getmyad', routing_key='advertise.delete')
-        ch_worker.close()
+        self.ch.basic_publish(msg, exchange='getmyad', routing_key='advertise.delete')
 
     def informer_rating_update(self):
-        ch_worker = self._get_worker_channel()
         msg = amqp.Message('')
-        ch_worker.basic_publish(msg, exchange='getmyad', routing_key='rating.informer')
-        ch_worker.close()
+        self.ch.basic_publish(msg, exchange='getmyad', routing_key='rating.informer')
 
     def campaign_rating_update(self):
-        ch_worker = self._get_worker_channel()
         msg = amqp.Message('')
-        ch_worker.basic_publish(msg, exchange='getmyad', routing_key='rating.campaign')
-        ch_worker.close()
+        self.ch.basic_publish(msg, exchange='getmyad', routing_key='rating.campaign')
 
     def offer_rating_update(self):
-        ch_worker = self._get_worker_channel()
         msg = amqp.Message('')
-        ch_worker.basic_publish(msg, exchange='getmyad', routing_key='rating.offer')
-        ch_worker.close()
+        self.ch.basic_publish(msg, exchange='getmyad', routing_key='rating.offer')
